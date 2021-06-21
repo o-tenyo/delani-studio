45dfc875e51754cb11af7c43ffad217cf6346149
o-tenyo <johnstone.omundi@student.moringaschool.comgit config --global user.email johnstone.omundi@student.moringaschool.com>
2021-06-21 18:04:29 +0300

    Finished adding jQuery functions in the 'what-we-do' section

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+//the first icon
     $('#fig1').click(function(){
         $('.design').toggle();
         $('.design-hidden').toggle();
@@ -6,5 +7,23 @@ $(document).ready(function(){
     $('.design-hidden').click(function(){
         $('.design-hidden').toggle();
         $('.design').toggle();
+    });
+//the second icon
+    $('#fig2').click(function(){
+        $('.development').toggle();
+        $('.development-hidden').toggle();
+    })
+    $('.development-hidden').click(function(){
+        $('.development-hidden').toggle();
+        $('.development').toggle();
+    });
+//the third icon
+    $('#fig3').click(function(){
+        $('.product').toggle();
+        $('.product-hidden').toggle();
+    })
+    $('.product-hidden').click(function(){
+        $('.product-hidden').toggle();
+        $('.product').toggle();
     });
 });
